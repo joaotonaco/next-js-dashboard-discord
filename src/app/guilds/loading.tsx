0388cf12e50cb284164f4ef8dddfda5b28ab8401
João Gabriel Tonaco <64219777+bluee-js@users.noreply.github.com>
2022-11-28 22:25:36 -0300

    Update loading.tsx

--- src/app/guilds/loading.tsx
+++ src/app/guilds/loading.tsx
@@ -7,8 +7,8 @@ function LoadingGuilds() {
       <div className="flex flex-col mx-auto items-start w-full gap-10">
         <div className="px-10 py-5 flex flex-col sm:flex-row items-center gap-2 shadow-md w-full bg-neutral-750 justify-center sm:justify-start">
           <GoGear size={30} />
-          <h1 className="font-semibold leading-8 text-[clamp(1.2rem,2vw+0.5rem,1.5rem)] text-center flex">
-            Configuring <VscLoading className="animate-spin" size={30} />
+          <h1 className="font-semibold leading-8 text-[clamp(1.2rem,2vw+0.5rem,1.5rem)] text-center flex gap-2">
+            Configuring <VscLoading className="animate-[spin_0.5s_linear_infinite]" size={30} />
           </h1>
         </div>
       </div>
